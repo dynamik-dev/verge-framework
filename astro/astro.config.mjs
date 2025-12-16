c7bcb99579f18d3b3fb166dfdb1ee543589ab170
Chris Arter <chris@arter.dev>
2025-12-16 14:43:44 -0500

    astro config

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'static',
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 	site: 'https://verge.dev',
 	vite: {
 		plugins: [tailwindcss()],
